330c5c7bf9148f6463f6c1cc7628089be79ea7c3
Kaiser-G <kaiserg6969@gmail.com>
2024-03-12 14:33:15 +0100

    test terminé

--- js/index.js
+++ js/index.js
@@ -3,21 +3,21 @@ import "../2024/node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
 // Import SweetAlert
 import Swal from "../2024/node_modules/sweetalert2/src/sweetalert2.js";
-
+//apparait a l'ouverture du site
 window.addEventListener("load", () => {
   Swal.fire({
-    title: "Erreur !",
+    title: "Bonjour",
     text: "Voulez-vous continuer ?",
     icon: "Erreur",
     confirmButtonText: "Oui",
   });
-
+  //Sur le bouton danger
   let btn = document.getElementById("btn");
   btn.addEventListener("click", () => {
     Swal.fire(
       {
-        title: "The Internet?",
-        text: "That thing is still around?",
+        title: "DANGER ?",
+        text: "Si pas danger, click",
         icon: "question",
       },
       false
